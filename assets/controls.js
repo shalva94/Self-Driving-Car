d122b21777d425a0aaebeea6912952279f2cdad0
shalva94 <69730500+shalva94@users.noreply.github.com>
2022-06-03 15:39:08 +0300

    Add files via upload

---
+++ assets/controls.js
@@ -0,0 +1,59 @@
+class Controls {
+    constructor(type) {
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
+
+        switch (type) {
+            case "KEYS":
+                this.addKeyBoardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    addKeyBoardListeners() {
+        document.onkeydown = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        document.onkeyup = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
